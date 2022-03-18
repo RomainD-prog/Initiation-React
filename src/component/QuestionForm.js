9ff6b67bf5ba7652b9126d621851570eb07ba275
RomainD-prog <rdurieux@ensc.fr>
2022-03-18 17:47:31 +0100

    avancée

---
+++ src/component/QuestionForm.js
@@ -0,0 +1,24 @@
+function handleSubmit(e) {
+    e.preventDefault()
+    alert(e.target['my_input'].value)
+}
+<form onSubmit={handleSubmit}>
+    <input type='text' name='my_input' defaultValue='Tapez votre texte' />
+    <button type='submit'>Entrer</button>
+</form>
+import { useState } from 'react'
+
+function QuestionForm() {
+    const [inputValue, setInputValue] = useState('Posez votre question ici')
+    return (
+        <div>
+            <textarea
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+         />
+            <button onClick={() => alert(inputValue)}>Alertez moi 🚨</button>
+        </div>
+    )
+}
+
+export default QuestionForm
